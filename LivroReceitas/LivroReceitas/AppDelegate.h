8e6b8842dc49f57d286a469622e6f885fc9d86a5
Victor Watanabe <victor_rs_watanabe@hotmail.com>
2014-03-31 14:00:24 -0300

    CriadoProjeto

---
+++ LivroReceitas/LivroReceitas/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LivroReceitas
+//
+//  Created by Victor Watanabe on 31/03/14.
+//  Copyright (c) 2014 KNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LivroReceitas/LivroReceitas/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LivroReceitas
+//
+//  Created by Victor Watanabe on 31/03/14.
+//  Copyright (c) 2014 KNA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
